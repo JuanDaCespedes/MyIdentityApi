4645de4638f46d3e9eaa7a0845f2a1a28a6dff21
JuanDaCespedes <johnd7.c@gmail.com>
2024-05-02 23:51:03 -0500

    Mediator pattern configuration

---
+++ MyIdentityApi.Api/Application/Commands/UserCommands/CreateUserCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+
+namespace MyIdentityApi.Api.Application.Commands.UserCommands;
+
+public class CreateUserCommand(
+    string firstName,
+    string lastName,
+    string email,
+    string password,
+    string countryCodePhoneNumber,
+    string phoneNumber)
+    : IRequest
+{
+    public string FirstName { get; private set; } = firstName;
+
+    public string LastName { get; private set; } = lastName;
+
+    public string Email { get; private set; } = email;
+
+    public string Password { get; private set; } = password;
+
+    public string CountryCodePhoneNumber { get; private set; } = countryCodePhoneNumber;
+
+    public string PhoneNumber { get; private set; } = phoneNumber;
+}
---
+++ MyIdentityApi.Api/Infrastructure/ApplicationServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace MyIdentityApi.Api.Infrastructure;
+
+public static class ApplicationServiceExtensions
+{
+    public static void AddMediator(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblyContaining<Program>();
+            cfg.Lifetime = ServiceLifetime.Scoped;
+        });
+    }
+}
